efec1f17ebb209254da033bfdcb102110a45ad55
XilefEel <felixlee175@gmail.com>
2026-01-03 20:11:56 +0700

    created project scaffold

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
